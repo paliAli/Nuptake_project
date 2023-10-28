9f9e98d98ab559e5e9b0c51551b329f07d460676
paliAli <pavlackovaa@gmail.com>
2023-10-28 22:48:59 +0200

    Update NDVI.R

--- GoogleEarthEngine/NDVI/NDVI.R
+++ GoogleEarthEngine/NDVI/NDVI.R
@@ -10,7 +10,7 @@ NDVI$system.time_start <- as.Date(NDVI$system.time_start, format = "%b %d,%Y")
 NDVI <- NDVI %>%
   rename(date = system.time_start)
 
-NDVI <- NDVI [which(NDVI$meanNDVI >= "0.15"), ]
+NDVI <- NDVI [which(NDVI$meanNDVI >= "0.1"), ]
 
 library(ggplot2)
 library(ggthemes) 
