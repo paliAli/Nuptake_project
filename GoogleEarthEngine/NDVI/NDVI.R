5ae342c14d32b8c3131d0c754c7f7063679f1937
paliAli <pavlackovaa@gmail.com>
2023-10-31 17:00:22 +0100

    update_NDVI

--- GoogleEarthEngine/NDVI/NDVI.R
+++ GoogleEarthEngine/NDVI/NDVI.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 setwd("C:\\Users\\pavla\\OneDrive\\Documents\\Nuptake_project\\Nuptake_final\\GoogleEarthEngine\\NDVI")
 
-NDVI <- read.csv ("C:\\Users\\pavla\\OneDrive\\Documents\\Nuptake_project\\Nuptake_final\\GoogleEarthEngine\\NDVI\\NDVI_scale10_cloud50.csv")
+NDVI <- read.csv ("C:\\Users\\pavla\\OneDrive\\Documents\\Nuptake_project\\Nuptake_final\\GoogleEarthEngine\\NDVI\\NDVI_scale10.csv")
 NDVI <- NDVI %>% na.omit()
 
 NDVI$system.time_start <- as.Date(NDVI$system.time_start, format = "%b %d,%Y")
@@ -26,10 +26,11 @@ NDVI_plot <- ggplot(data = NDVI, aes(x = date, y = meanNDVI))+
   theme_minimal() +
   scale_x_date(date_labels = "%Y/%m", date_breaks = "2 months")+
   theme_minimal()+
-  theme(plot.margin = margin(8, 20, 5, 5),
-        axis.text.x = element_text(size = 11, angle = 35),
+  theme(plot.margin = margin(8, 30, 5, 5),
+        axis.text.x = element_text(size = 12, angle = 35),
         axis.text.y = element_text(size = 14),
         axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        axis.title.x = element_text(size = 15),
         plot.title = element_text (margin = margin (b = 20), size = 30))
 
 NDVI_plot
@@ -46,14 +47,13 @@ LAI_NDVI <- fuzzy_inner_join(mean_LAI_values, NDVI,
   mutate(date_difference = abs(difftime(Date, date, units = "days")))%>%
   arrange(date_difference, decreasing = FALSE)
 
-LAI_NDVI <- LAI_NDVI [-c(8,18), ]
+LAI_NDVI <- LAI_NDVI [-c(16,18), ]
 
 LAI_NDVI <- LAI_NDVI %>% group_by(Date)%>%
   summarise(mean_LAI = first(mean_LAI), meanNDVI = first(meanNDVI))
 
 LAI_NDVI <- LAI_NDVI [which(LAI_NDVI$Date <= "2023-06-01"), ]
-#remove values around the cut on 2022-05-14 
-LAI_NDVI <- LAI_NDVI [-5, ]
+
 
 linear_model <- lm(mean_LAI ~ meanNDVI, data = LAI_NDVI)
 summary(linear_model)
@@ -71,23 +71,25 @@ linear_LAI_NDVI_plot <- LAI_NDVI %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "NDVI", y = "LAI", title = "NDVI vs LAI")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
-           x = min(LAI_NDVI$meanNDVI) + 0.02, 
-           y = max(LAI_NDVI$mean_LAI) - 0.3,
+           x = min(LAI_NDVI$meanNDVI) + 0.05, 
+           y = max(LAI_NDVI$mean_LAI) - 0.5,
            label = paste("y =", format(slope, digits = 2), 
                          "*x +", 
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(LAI_NDVI$meanNDVI, LAI_NDVI$mean_LAI), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 
 linear_LAI_NDVI_plot
+
+
 
 
 # Plot CropHeight vs NDVI -------------------------------------------------
@@ -96,11 +98,8 @@ CropHeight_NDVI <- fuzzy_inner_join(mean_field_CropHeight, NDVI,
                                     by = c("Date" = "date"),
                                     match_fun = function(x, y) abs(difftime(x, y, units = "days")) <= 7) %>%
   mutate(date_difference = abs(difftime(Date, date, units = "days")))%>%
-  arrange(date_difference, decreasing = FALSE)
-
-CropHeight_NDVI <- CropHeight_NDVI [-6, ]
-
-CropHeight_NDVI <- CropHeight_NDVI %>% group_by(Date)%>%
+  arrange(date_difference, decreasing = FALSE)%>% 
+  group_by(Date)%>%
   summarise(`mean_height(cm)` = first(`mean_height(cm)`), meanNDVI = first(meanNDVI))
 
 
@@ -124,11 +123,11 @@ linear_NDVI_CH_plot <- CropHeight_NDVI %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "NDVI", y = "Crop height (cm)", title = "NDVI vs Crop height")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
            x = min(CropHeight_NDVI$meanNDVI) + 0.02, 
            y = max(CropHeight_NDVI$`mean_height(cm)`) - 20,
@@ -137,7 +136,7 @@ linear_NDVI_CH_plot <- CropHeight_NDVI %>%
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(CropHeight_NDVI$meanNDVI, CropHeight_NDVI$`mean_height(cm)`), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 linear_NDVI_CH_plot
 
@@ -151,7 +150,7 @@ Ncontent_NDVI <- fuzzy_inner_join(mean_Nuptake, NDVI,
   mutate(date_difference = abs(difftime(date.x, date.y, units = "days")))%>%
   arrange(date_difference, decreasing = FALSE)
 
-Ncontent_NDVI <- Ncontent_NDVI[-c(3,12), ]
+Ncontent_NDVI <- Ncontent_NDVI[-11, ]
 
 Ncontent_NDVI <- Ncontent_NDVI%>%
   group_by(date.x)%>%
@@ -173,20 +172,20 @@ Ncontent_NDVI_plot <- Ncontent_NDVI %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "NDVI", y = "N content (%)", title = "NDVI vs N content")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
-           x = min(Ncontent_NDVI$meanNDVI) + 0.1, 
+           x = min(Ncontent_NDVI$meanNDVI) + 0.14, 
            y = max(Ncontent_NDVI$mean_Ncontent) - 0.3,
            label = paste("y =", format(slope, digits = 2), 
                          "*x +", 
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(Ncontent_NDVI$meanNDVI, Ncontent_NDVI$mean_Ncontent), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 Ncontent_NDVI_plot
 
@@ -199,7 +198,7 @@ biomass_NDVI <- fuzzy_inner_join(mean_Nuptake, NDVI,
   mutate(date_difference = abs(difftime(date.x, date.y, units = "days")))%>%
   arrange(date_difference, decreasing = FALSE)
 
-biomass_NDVI <- biomass_NDVI [-c(3,12),]
+biomass_NDVI <- biomass_NDVI [-11,]
 
 biomass_NDVI<- biomass_NDVI %>%  
   group_by(date.x)%>%
@@ -222,19 +221,19 @@ biomass_NDVI_plot <- biomass_NDVI %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "NDVI", y = "biomass weight (g)", title = "NDVI vs biomass weight")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
-           x = min(biomass_NDVI$meanNDVI) + 0.05, 
-           y = max(biomass_NDVI$biomass_weight) - 0.5,
+           x = min(biomass_NDVI$meanNDVI) + 0.07, 
+           y = max(biomass_NDVI$biomass_weight) - 7,
            label = paste("y =", format(slope, digits = 2), 
                          "*x +", 
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(biomass_NDVI$meanNDVI, biomass_NDVI$biomass_weight), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 biomass_NDVI_plot
