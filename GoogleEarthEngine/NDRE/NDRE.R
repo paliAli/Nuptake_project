eaa5dd4802a63479ffc84dcd281f4127670e9f5a
paliAli <pavlackovaa@gmail.com>
2023-11-02 10:56:43 +0100

    NDRE_update

--- GoogleEarthEngine/NDRE/NDRE.R
+++ GoogleEarthEngine/NDRE/NDRE.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 setwd("C:\\Users\\pavla\\OneDrive\\Documents\\Nuptake_project\\Nuptake_final\\GoogleEarthEngine")
 
-NDRE <- read.csv ("C:\\Users\\pavla\\OneDrive\\Documents\\Nuptake_project\\Nuptake_final\\GoogleEarthEngine\\NDRE\\NDRE_scale10_cloud50.csv")
+NDRE <- read.csv ("C:\\Users\\pavla\\OneDrive\\Documents\\Nuptake_project\\Nuptake_final\\GoogleEarthEngine\\NDRE\\NDRE_scale10.csv")
 NDRE <- NDRE %>% na.omit()%>%
   rename(date = 'system.time_start')
 
@@ -26,8 +26,8 @@ NDRE_plot <- ggplot(data = NDRE, aes(x = date, y = meanNDRE))+
   theme_minimal() +
   scale_x_date(date_labels = "%b/%Y", date_breaks = "2 months")+
   theme_minimal()+
-  theme(plot.margin = margin(8, 20, 5, 5),
-        axis.text.x = element_text(size = 14, angle = 35),
+  theme(plot.margin = margin(8, 30, 5, 5),
+        axis.text.x = element_text(size = 12, angle = 35),
         axis.text.y = element_text(size = 14),
         axis.title.x = element_text(margin = margin(t = 20), size = 15),
         axis.title.y = element_text(margin = margin(r = 20), size = 15),
@@ -36,6 +36,7 @@ NDRE_plot <- ggplot(data = NDRE, aes(x = date, y = meanNDRE))+
 NDRE_plot
 
 # NDRE vs LAI -------------------------------------------------------------
+library(fuzzyjoin)
 
 mean_LAI_NDRE <- fuzzy_inner_join(mean_LAI_values, NDRE, 
                                    by = c("Date" = "date"),
@@ -43,7 +44,7 @@ mean_LAI_NDRE <- fuzzy_inner_join(mean_LAI_values, NDRE,
   mutate(date_difference = abs(difftime(Date, date, units = "days")))%>%
   arrange(date_difference, decreasing = FALSE)
 
-mean_LAI_NDRE <- mean_LAI_NDRE [-c(7,15), ]
+mean_LAI_NDRE <- mean_LAI_NDRE [-c(16,18), ]
 
 mean_LAI_NDRE <- mean_LAI_NDRE %>% group_by(Date)%>%
   summarise(mean_LAI = first(mean_LAI), meanNDRE = first(meanNDRE))
@@ -68,20 +69,20 @@ linear_LAI_NDRE_plot <- mean_LAI_NDRE %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "NDRE", y = "LAI", title = "NDRE vs LAI")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
-           x = min(mean_LAI_NDRE$meanNDRE) + 0.02, 
+           x = min(mean_LAI_NDRE$meanNDRE) + 0.07, 
            y = max(mean_LAI_NDRE$mean_LAI) - 0.3,
            label = paste("y =", format(slope, digits = 2), 
                          "*x +", 
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(mean_LAI_NDRE$meanNDRE, mean_LAI_NDRE$mean_LAI), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 linear_LAI_NDRE_plot
 
@@ -91,14 +92,12 @@ CropHeight_NDRE <- fuzzy_inner_join(mean_field_CropHeight, NDRE,
                                      by = c("Date" = "date"),
                                      match_fun = function(x, y) abs(difftime(x, y, units = "days")) <= 7) %>%
   mutate(date_difference = abs(difftime(Date, date, units = "days")))%>%
-  arrange(date_difference, decreasing = FALSE)
-
-CropHeight_NDRE <- CropHeight_NDRE [-4, ]
-
-CropHeight_NDRE <- CropHeight_NDRE %>%  group_by(Date)%>%
+  arrange(date_difference, decreasing = FALSE)%>%  
+  group_by(Date)%>%
   summarise(`mean_height(cm)` = first(`mean_height(cm)`), meanNDRE = first(meanNDRE))
 
-
+#remove values around the cuts on 2022-05-14, probably lower NDVI due to flowering
+CropHeight_NDRE <- CropHeight_NDRE [-5, ]
 CropHeight_NDRE <- CropHeight_NDRE [which(CropHeight_NDRE$Date <= "2023-06-01"), ]
 
 
@@ -118,20 +117,20 @@ linear_CH_NDRE_plot <- CropHeight_NDRE %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "NDRE", y = "Crop height (cm)", title = "NDRE vs Crop height")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
-           x = min(CropHeight_NDRE$meanNDRE) + 0.01, 
+           x = min(CropHeight_NDRE$meanNDRE) + 0.04, 
            y = max(CropHeight_NDRE$`mean_height(cm)`) - 10,
            label = paste("y =", format(slope, digits = 2), 
                          "*x +", 
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(CropHeight_NDRE$meanNDRE, CropHeight_NDRE$`mean_height(cm)`), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 
 
@@ -140,15 +139,13 @@ linear_CH_NDRE_plot
 
 # NDRE vs N content -----------------------------------------------------
 
-library(fuzzyjoin)
-
 Ncontent_NDRE <- fuzzy_inner_join(mean_Nuptake, NDRE, 
                                    by = c("date" = "date"),
                                    match_fun = function(x, y) abs(difftime(x, y, units = "days")) <= 7) %>%
   mutate(date_difference = abs(difftime(date.x, date.y, units = "days")))%>%
   arrange(date_difference, decreasing = FALSE)
 
-Ncontent_NDRE <- Ncontent_NDRE[-c(3,11), ]
+Ncontent_NDRE <- Ncontent_NDRE[-c(9,11), ]
 
 Ncontent_NDRE <- Ncontent_NDRE%>%
   group_by(date.x)%>%
@@ -170,20 +167,20 @@ Ncontent_NDRE_plot <- Ncontent_NDRE %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "NDRE", y = "N content (%)", title = "NDRE vs N content")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
-           x = min(Ncontent_NDRE$meanNDRE) + 0.12, 
-           y = max(Ncontent_NDRE$mean_Ncontent) - 0.4,
+           x = min(Ncontent_NDRE$meanNDRE) + 0.15, 
+           y = max(Ncontent_NDRE$mean_Ncontent) - 0.2,
            label = paste("y =", format(slope, digits = 2), 
                          "*x +", 
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(Ncontent_NDRE$meanNDRE, Ncontent_NDRE$mean_Ncontent), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 Ncontent_NDRE_plot
 
@@ -197,7 +194,7 @@ biomass_NDRE <- fuzzy_inner_join(mean_Nuptake, NDRE,
   mutate(date_difference = abs(difftime(date.x, date.y, units = "days")))%>%
   arrange(date_difference, decreasing = FALSE)
 
-biomass_NDRE <- biomass_NDRE [-c(3,11),]
+biomass_NDRE <- biomass_NDRE [-c(9,11),]
   
 biomass_NDRE<- biomass_NDRE %>%  
   group_by(date.x)%>%
@@ -220,20 +217,20 @@ biomass_NDRE_plot <- biomass_NDRE %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "NDRE", y = "biomass weight (g)", title = "NDRE vs biomass weight")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
-           x = min(biomass_NDRE$meanNDRE) + 0.05, 
-           y = max(biomass_NDRE$biomass_weight) - 0.5,
+           x = min(biomass_NDRE$meanNDRE) + 0.03, 
+           y = max(biomass_NDRE$biomass_weight) - 5,
            label = paste("y =", format(slope, digits = 2), 
                          "*x +", 
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(biomass_NDRE$meanNDRE, biomass_NDRE$biomass_weight), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 biomass_NDRE_plot
 
