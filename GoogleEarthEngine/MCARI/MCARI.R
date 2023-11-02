ba8e86fbcaa5976bfb06810fc6400fa3826eca01
paliAli <pavlackovaa@gmail.com>
2023-11-02 11:57:44 +0100

    update MCARI

--- GoogleEarthEngine/MCARI/MCARI.R
+++ GoogleEarthEngine/MCARI/MCARI.R
@@ -1,8 +1,8 @@
 library (data.table)
 library(dplyr)
 
-setwd("C:\\Users\\apavlackova\\Documents\\GoogleEarthEngine\\MCARI")
-MCARI <- read.csv ("C:\\Users\\pavla\\OneDrive\\Documents\\Nuptake_project\\Nuptake_final\\GoogleEarthEngine\\MCARI\\MCARI_scale10_cloud50.csv")
+setwd("C:\\Users\\pavla\\OneDrive\\Documents\\GitHub\\Nuptake_project\\GoogleEarthEngine")
+MCARI <- read.csv ("C:\\Users\\pavla\\OneDrive\\Documents\\Nuptake_project\\Nuptake_final\\GoogleEarthEngine\\MCARI\\MCARI_scale10.csv")
 MCARI <- MCARI %>% na.omit() %>%
   rename(date = 'system.time_start')
 
@@ -11,7 +11,7 @@ MCARI$date <- as.Date(MCARI$date, format = "%b %d,%Y")
 library(ggplot2)
 library(ggthemes)
 
-MCARI <- MCARI [which(MCARI$meanMCARI >= 0.05), ]
+MCARI <- MCARI [which(MCARI$meanMCARI >= 0.01), ]
 
 ggplot(data = MCARI, aes(x = date, y = meanMCARI))+
   geom_point(size =2, color = "darkgreen")+
@@ -22,10 +22,11 @@ ggplot(data = MCARI, aes(x = date, y = meanMCARI))+
   theme_minimal() +
   scale_x_date(date_labels = "%Y/%m", date_breaks = "2 months")+
   theme_minimal()+
-  theme(plot.margin = margin(8, 20, 5, 5),
-        axis.text.x = element_text(size = 11, angle = 35),
+  theme(plot.margin = margin(8, 30, 5, 5),
+        axis.text.x = element_text(size = 12, angle = 35),
         axis.text.y = element_text(size = 14),
         axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        axis.title.x = element_text(size = 15),
         plot.title = element_text (margin = margin (b = 20), size = 30))
 
 library(fuzzyjoin)
@@ -39,7 +40,7 @@ mean_LAI_MCARI <- fuzzy_inner_join(mean_LAI_values, MCARI,
   mutate(date_difference = abs(difftime(Date, date, units = "days")))%>%
   arrange(date_difference, decreasing = FALSE)
 
-mean_LAI_MCARI <- mean_LAI_MCARI [-c(7,15), ]
+mean_LAI_MCARI <- mean_LAI_MCARI [-18, ]
 
 mean_LAI_MCARI <- mean_LAI_MCARI %>% group_by(Date)%>%
   summarise(mean_LAI = first(mean_LAI), meanMCARI = first(meanMCARI))
@@ -66,20 +67,20 @@ linear_LAI_MCARI_plot <- mean_LAI_MCARI %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "MCARI", y = "LAI", title = "MCARI vs LAI")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
-           x = min(mean_LAI_MCARI$meanMCARI) + 0.005, 
-           y = max(mean_LAI_MCARI$mean_LAI) - 0.6,
+           x = min(mean_LAI_MCARI$meanMCARI) + 0.08, 
+           y = max(mean_LAI_MCARI$mean_LAI),
            label = paste("y =", format(slope, digits = 2), 
                          "*x +", 
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(mean_LAI_MCARI$meanMCARI, mean_LAI_MCARI$mean_LAI), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 
 
@@ -93,13 +94,9 @@ CropHeight_MCARI <- fuzzy_inner_join(mean_field_CropHeight, MCARI,
                                     by = c("Date" = "date"),
                                     match_fun = function(x, y) abs(difftime(x, y, units = "days")) <= 7) %>%
   mutate(date_difference = abs(difftime(Date, date, units = "days")))%>%
-  arrange(date_difference, decreasing = FALSE)
-
-CropHeight_MCARI <- CropHeight_MCARI[-4, ]
-
-CropHeight_MCARI <- CropHeight_MCARI %>% group_by(Date)%>%
+  arrange(date_difference, decreasing = FALSE)%>% 
+  group_by(Date)%>%
   summarise(`mean_height(cm)` = first(`mean_height(cm)`), meanMCARI = first(meanMCARI))
-
 
 CropHeight_MCARI <- CropHeight_MCARI [which(CropHeight_MCARI$Date <= "2023-06-01"), ]
 
@@ -120,11 +117,11 @@ linear_CH_MCARI_plot <- CropHeight_MCARI %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "MCARI", y = "Crop height (cm)", title = "MCARI vs Crop height")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
            x = min(CropHeight_MCARI$meanMCARI) + 0.005, 
            y = max(CropHeight_MCARI$`mean_height(cm)`) - 10,
@@ -133,7 +130,7 @@ linear_CH_MCARI_plot <- CropHeight_MCARI %>%
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(CropHeight_MCARI$meanMCARI, CropHeight_MCARI$`mean_height(cm)`), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 
 
@@ -148,7 +145,7 @@ Ncontent_MCARI <- fuzzy_inner_join(mean_Nuptake, MCARI,
   mutate(date_difference = abs(difftime(date.x, date.y, units = "days")))%>%
   arrange(date_difference, decreasing = FALSE)
 
-Ncontent_MCARI <- Ncontent_MCARI [-c(3,11),]
+Ncontent_MCARI <- Ncontent_MCARI [-c(9,11),]
 
 Ncontent_MCARI <- Ncontent_MCARI%>%
   group_by(date.x)%>%
@@ -169,11 +166,11 @@ Ncontent_MCARI_plot <- Ncontent_MCARI %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "MCARI", y = "N content (%)", title = "MCARI vs N content")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
            x = min(Ncontent_MCARI$meanMCARI) + 0.12, 
            y = max(Ncontent_MCARI$mean_Ncontent) - 0.1,
@@ -182,7 +179,7 @@ Ncontent_MCARI_plot <- Ncontent_MCARI %>%
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(Ncontent_MCARI$meanMCARI, Ncontent_MCARI$mean_Ncontent), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 Ncontent_MCARI_plot
 
@@ -196,7 +193,7 @@ biomass_MCARI <- fuzzy_inner_join(mean_Nuptake, MCARI,
   mutate(date_difference = abs(difftime(date.x, date.y, units = "days")))%>%
   arrange(date_difference, decreasing = FALSE)
 
-biomass_MCARI <- biomass_MCARI[-c(3,11),]
+biomass_MCARI <- biomass_MCARI[-c(9,11),]
 
 biomass_MCARI <- biomass_MCARI%>%
   group_by(date.x)%>%
@@ -219,11 +216,11 @@ biomass_MCARI_plot <- biomass_MCARI %>%
   geom_smooth(method = lm, se = FALSE)+
   labs(x = "MCARI", y = "biomass weight (g)", title = "MCARI vs biomass weight")+
   theme_minimal()+
-  theme(axis.text.x = element_text(size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(margin = margin(t = 20), size = 12),
-        axis.title.y = element_text(margin = margin(r = 20), size = 12),
-        plot.title = element_text (margin = margin (b = 20), size = 20))+
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(margin = margin(t = 20), size = 15),
+        axis.title.y = element_text(margin = margin(r = 20), size = 15),
+        plot.title = element_text (margin = margin (b = 20), size = 22))+
   annotate("text",
            x = min(biomass_MCARI$meanMCARI) + 0.1, 
            y = max(biomass_MCARI$biomass_weight) - 3,
@@ -232,7 +229,7 @@ biomass_MCARI_plot <- biomass_MCARI %>%
                          format(intercept, digits = 2),
                          "\nR2 =", round(r_squared, 2),
                          "\nCorrelation:", round(cor(biomass_MCARI$meanMCARI, biomass_MCARI$biomass_weight), 2)),
-           hjust = 0, vjust = 1, color = "black", size = 4)
+           hjust = 0, vjust = 1, color = "black", size = 6)
 
 biomass_MCARI_plot
 
